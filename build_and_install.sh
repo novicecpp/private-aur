1b50462a9b4acfc03cbc61396877b7ed26bffc31
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-06-28 23:24:17 +0200

    always true

--- build_and_install.sh
+++ build_and_install.sh
@@ -3,16 +3,16 @@
 # TODO: convert to ansible module later
 
 set -euo pipefail
+set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-set -x
 
 pacman_package_name=$1
 echo "Package name: $pacman_package_name"
 pushd $SCRIPT_DIR/${pacman_package_name}
 
 build_package_ver=$( ( source PKGBUILD; echo $pkgver-$pkgrel ) )
-installed_package_ver=$(pacman -Qim $pacman_package_name  | grep Version | awk -F':' '{print $2}' | awk '{print $1}')
+installed_package_ver=$(pacman -Qim $pacman_package_name  | grep Version | awk -F':' '{print $2}' | awk '{print $1}') || true
 
 if [[ $build_package_ver == $installed_package_ver ]]; then
     echo "Package is already installed."
